50cc2dbb557efb46addcad6dcc2ae9334417dabb
IvyMasterDiego <diegolobato82@gmail.com>
2021-11-05 21:45:34 -0400

    first commit

---
+++ AplicacionEnClase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionEnClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int limit = 10;
+            int auxiliar;
+            int primo = 2;
+            for (int i = 0; i < limit; i++)
+            {
+                primo++;
+                auxiliar = a;
+                a = b;
+                b = auxiliar + a;
+                //Console.WriteLine(a);
+                if ((a % 2) == 0 && a % primo == 0)
+                {
+                    Console.WriteLine(a + " || Par || Primo");
+                }
+                else
+                {
+                    Console.WriteLine(a + "|| Impar || No es primo");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
